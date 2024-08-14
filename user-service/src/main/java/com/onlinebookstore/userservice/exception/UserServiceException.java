1967b2da9545b350a71ac77bce814e27cd26f38d
Girdharilal12 <girdharilal2000@outlook.com>
2024-08-14 18:26:54 +0500

    Improved clarity and maintainability of error messages in user service class & add custom excaption

---
+++ user-service/src/main/java/com/onlinebookstore/userservice/exception/UserServiceException.java
@@ -0,0 +1,7 @@
+package com.onlinebookstore.userservice.exception;
+
+public class UserServiceException extends RuntimeException{
+    public UserServiceException(String message) {
+        super(message);
+    }
+}
